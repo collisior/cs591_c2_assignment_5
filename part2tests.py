c5607ae48fd563ea320938443228cceef55865c8
Camilla Satte <camillasatte@gmail.com>
2019-10-16 23:53:53 -0400

    Final fix

--- part2tests.py
+++ part2tests.py
@@ -1,31 +1,32 @@
 import unittest 
-import part2
+from part2 import *
 import math as math
 
 class ShapeTest(unittest.TestCase):
+    print("Hello")
 
-    def text_test(self):
+    def testText(self):
         s = ShapeCalculator()
         self.assertEqual(s.askUser(), 'Choose a shape: (C)ircle or (R)ectangle or (S)quare')
 
-    def shape_test(self):
+    def testShape(self):
         s = ShapeCalculator()
         self.assertEqual(s.whichShape('R'),'Rectangle')
         self.assertEqual(s.whichShape('C'),'Circle')
         self.assertEqual(s.whichShape('S'),'Square')
     
-    def questionTest(self):
+    def testQuestion(self):
         s = ShapeCalculator()
         self.assertEqual(s.getQuest('R'), "Rectangle length?"+"Rectangle Breadth?")
         self.assertEqual(s.getQuest('C'), "Circle Circumference?")
         self.assertEqual(s.getQuest('S'), "Square Breadth?")
     
-    def measurementTests(self):
+    def testMeasurement(self):
         s = ShapeCalculator()
         self.assertEqual(s.getRectMeasure(4, 5), 20)
         self.assertEqual(s.getCircletMeasure(6), (9)*math.pi)
         self.assertEqual(s.getSquareMeasure(6), 36)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
-    
+
